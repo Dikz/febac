da2bfc488b1c669fb32d027a184d1bb5516a2110
Dikz <cicerogabriel12@hotmail.com>
2018-03-16 14:39:24 -0300

    Final da Questao1 TDE6

--- Projeto C/TDE6/primeiraseg.cpp
+++ Projeto C/TDE6/primeiraseg.cpp
@@ -3,19 +3,18 @@
 
 int main(){
     int valor;
-    
+
     do{
         printf("Insira um valor: ");
         scanf("%d",&valor);
         if(valor >= 5){
-                 printf("Entrada Invalida\n");
+                 printf("\n>>Entrada Invalida\n");
         }
         else {
-             printf("Tudo OK... Voce digitou %d\n\n",valor);
-             system("pause");     
+             printf("\n\tTudo OK... Voce digitou o numero %d\n\n\n",valor);
         }
     }
-    
+
     while(valor >=5);
-    return 0;
+    system("pause");
 }

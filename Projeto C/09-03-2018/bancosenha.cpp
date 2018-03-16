410402f09bcbb13fa6ce5b3116e935ad51f13080
Dikz <cicerogabriel12@hotmail.com>
2018-03-16 14:57:09 -0300

    Final Banco Senha

--- Projeto C/09-03-2018/bancosenha.cpp
+++ Projeto C/09-03-2018/bancosenha.cpp
@@ -3,24 +3,25 @@
 
 int main(){
     int senha=0, cont=0;
-    
-    printf("ENTRE COM SUA SENHA: ");
+
+    printf("\t@FEBAC - Bank\n\t_____________\n\n");
+    printf("\tENTRE COM SUA SENHA: ");
     scanf("%d",&senha);
-    
+
        while(senha != 123){
-         printf("Digite sua senha novamente: ");
+         printf("\n>>Digite sua senha novamente: ");
          scanf("%d",&senha);
          cont = cont +1;
-         
+
            if(cont == 3 && senha != 123){
-                   printf("Senha Bloqueada.\n\n");
-                   break;     
-           }            
-       }    
-       
+                   printf("\n\t----------------\n\tSenha Bloqueada.\n\t----------------\n\n\n");
+                   break;
+           }
+       }
+
        if(senha == 123){
-                printf("Acessado com sucesso!\n\n");
-                printf("Quandidade de loops: %d\n\n",cont);         
+                printf("\n\t----------------------\n\t# Acessado com sucesso!\n\t----------------------\n\n");
+                printf("Quandidade de tentativas(loops): %d\n\n",cont);
        }
        system("pause");
 }

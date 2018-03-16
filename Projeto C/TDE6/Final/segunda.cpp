7cf259f7f5a7ffbdffbf0cbc8fdb2e1f1452875d
Dikz <cicerogabriel12@hotmail.com>
2018-03-16 20:37:17 -0300

    Finalizacao TDE6 Segunda Questao

---
+++ Projeto C/TDE6/Final/segunda.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int valor;
+    
+    printf("Insira um valor: ");
+    scanf("%d",&valor);
+        
+        if(valor >= 100 && valor <= 200){
+                  printf("Voce digitou um numero na faixa de 100 a 200!\n\n");
+        }
+        else {
+             printf("Voce digitou um numero fora da faixa de 100 a 200\n\n");     
+        }
+    
+    system("pause");  
+}
